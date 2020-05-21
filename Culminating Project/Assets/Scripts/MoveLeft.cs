00112dbef8757ee1cb14251c4bffaf1b671517d9
katherinenemits <59098985+katherinenemits@users.noreply.github.com>
2020-05-21 12:51:07 -0700

    Scene, Player controller script in progress

---
+++ Culminating Project/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets the player controller script
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // moves the object and background to the left
+    void Update()
+    {
+        //gets the status of game over; if true then it stops the game
+        if(playerControllerScript.gameOver == false)
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        //destrows obstacles that go out of the game view
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Culminating Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // allows the player to jump
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // only allows the player to jump once at a time
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            //checks if player is on the ground
+            isOnGround = true;
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            //end statement
+            gameOver = true;
+            Debug.Log("Game Over!");
+            //makes the player stop running
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
